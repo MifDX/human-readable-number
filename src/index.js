b6f793c93cb06eb158563134d78b1c5f7e601540
MifDX <104031797+MifDX@users.noreply.github.com>
2023-01-24 23:18:41 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+let one_nineteen = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+let twenty_ninety = "'' '' twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+if (number < 20) 
+    return one_nineteen[number];
+let digit = number%10;
+if (number < 100) 
+    return twenty_ninety[~~(number/10)] + (digit? " " + one_nineteen[digit]: "");
+if (number < 1000) 
+    return one_nineteen[~~(number/100)] +" hundred" + (number%100 == 0? "": " " + toReadable(number%100)); 
 }
+
